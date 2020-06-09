43b585333c61219bbf2b98b688904ffcdb2a9999
lijiajia <lijiajia_2019@163.com>
2020-06-09 23:22:30 +0800

    链表

---
+++ src/Tree/Node.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C), 2019-2020, XXX有限公司
+ * FileName: Node
+ * Author:   李佳佳同学
+ * Date:     2020/6/9 23:21
+ * Description: 链表
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package Tree;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈链表〉
+ *
+ * @author 李佳佳同学
+ * @create 2020/6/9
+ * @since 1.0.0
+ */
+public class Node {
+
+
+    Node left;
+    Node right;
+    int val;
+
+
+
+}
